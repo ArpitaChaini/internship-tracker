67048f9d69865c14331a1923a2d0e8b2e2e40aa5
Arpita Meghamala Chaini <arpitachaini098@gmail.com>
2026-03-08 13:59:54 +0530

    fix mysql port

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,12 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,   // ADD THIS LINE
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "localhost",
+  user: "root",
+  password: "Root@123",
+  database: "internship_tracker",
   waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  connectionLimit: 10
 });
 
 module.exports = pool;
